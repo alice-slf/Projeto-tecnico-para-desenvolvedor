c2fdd57f9dcda3b467f4bb229bd7f026c4a5f7b6
Alice Santos <alice.slfaria@gmail.com>
2020-10-16 18:04:00 -0300

    criando tabela autor no banco de dados com sucesso

---
+++ database/migrations/2020_10_16_205847_create_autor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAutorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('autor', function (Blueprint $table) {
+            $table->string('nome');
+            $table->primary('nome');
+            $table->date('ano_Nasc');
+            $table->string('sexo');
+            $table->string('nacionalidade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('autor');
+    }
+}
