2d6ba2b540283f09bba2131c1db61c1c4a37dac4
Alice Santos <alice.slfaria@gmail.com>
2020-10-17 22:04:43 -0300

    Criação da tabela 'livros' no banco de dados

---
+++ database/migrations/2020_10_18_005934_create_livros_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLivrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string('autor');
+            $table->string('genero_Literario');
+            $table->string('editora');
+            $table->string('titulo');
+            $table->date('ano_Lancamento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livros');
+    }
+}
