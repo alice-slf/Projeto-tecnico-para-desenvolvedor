39bc784b7fbece768556f1964116eb59a296fc5f
Alice Santos <alice.slfaria@gmail.com>
2020-10-19 23:35:08 -0300

    Adicionando pastas do projeto

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Biblioteca Virtual</h1><br />
+    <ul>
+      <h3>Cadastre, Liste, Atualize e Exclua</h3><br />
+    </ul>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Editora</a></li><br />
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Gênero Literário</a></li><br />
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Autor</a></li><br />
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Livro</a></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #000405;
+}
+</style>
